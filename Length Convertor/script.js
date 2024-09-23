289b8347ba32d5aa4d402e699125ae27442d631d
MFM Asadullah <156198964+AsdTechy@users.noreply.github.com>
2024-09-22 23:03:32 -0700

    Add files via upload

---
+++ Length Convertor/script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    let cmVal = Number(document.getElementById("input").value);
+    let inchVal = cmVal/2.54;
+    let result = document.getElementById("result");
+    result.innerHTML = inchVal.toFixed(2) + ' Inches';
+}
